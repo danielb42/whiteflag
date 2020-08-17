2174113e3eefaf6f15e5070cc8dea7e2d3c32f7e
danielb <git@masterunit.net>
2020-08-17 12:43:28 +0200

    add deprecation comments

--- compat.go
+++ compat.go
@@ -1,22 +1,22 @@
 package whiteflag
 
-// ParseCommandLine is a no-op and only kept for backward compatibility.
+// (Deprecated) ParseCommandLine is a no-op and only kept for backward compatibility.
 func ParseCommandLine() {}
 
-// CheckBool returns true when flag is present on the command line.
+// (Deprecated) CheckBool returns true when flag is present on the command line.
 // It is deprecated, use FlagPresent().
 func CheckBool(flag string) bool {
 	return FlagPresent(flag)
 }
 
-// CheckInt returns true when flag is present on the command line and
+// (Deprecated) CheckInt returns true when flag is present on the command line and
 // is followed by an integer value.
 // It is deprecated, use FlagPresent().
 func CheckInt(flag string) bool {
 	return FlagPresent(flag)
 }
 
-// CheckString returns true when flag is present on the command line and
+// (Deprecated) CheckString returns true when flag is present on the command line and
 // is followed by a string value.
 // It is deprecated, use FlagPresent().
 func CheckString(flag string) bool {
